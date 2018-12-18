a74b3442b6ec05cc93536f57b48d179fc4cbff66
vincespokes <vince@sparkdevnetwork.org>
2018-12-18 16:12:29 -0700

    local commit added project reference directly to Rock.RestClient

--- Applications/Wpf/CheckScannerUtility/CheckScannerUtility/BatchPage.xaml.cs
+++ Applications/Wpf/CheckScannerUtility/CheckScannerUtility/BatchPage.xaml.cs
@@ -20,7 +20,6 @@ using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using Rock.Client;
