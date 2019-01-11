d43b7d8fa58303e3755ef56154e9448173246775
vincespokes <vince@sparkdevnetwork.org>
2019-01-10 18:25:07 -0700

    Checkpoint Started Layout

---
+++ Applications/Wpf/Rock.Wpf/Commands/WindowCloseCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Rock.Wpf
+{
+    public class WindowCloseCommand : ICommand
+    {
+
+        public bool CanExecute( object parameter )
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute( object parameter )
+        {
+            var window = parameter as Window;
+
+            if ( window != null )
+            {
+                window.Close();
+            }
+        }
+    }
+}
+
---
+++ Applications/Wpf/Rock.Wpf/Commands/WindowRestoreCommand.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Rock.Wpf
+{
+    public class WindowRestoreCommand : ICommand
+    {
+        public bool CanExecute( object parameter )
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute( object parameter )
+        {
+            var window = parameter as Window;
+
+            if ( window != null )
+            {
+                if ( window.WindowState == WindowState.Maximized )
+                {
+                    //TODO Pull in Users Last Save Sized
+                    window.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    window.WindowState = WindowState.Maximized;
+                }
+            }
+        }
+    }
+}
